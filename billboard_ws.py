eaa94cac39d4bcc400b151988a8898abd80e5660
holliske <holliske@sas.upenn.edu>
2018-03-29 17:36:49 -0400

    Update billboard_ws.py

--- billboard_ws.py
+++ billboard_ws.py
@@ -7,19 +7,19 @@ year = []
 artist_name = []
 rank = []
 
-count = 10
-while chart.date and count > 0:
+count = 0
+while chart.date:
 	time.sleep(1)
 	for song in chart:
 		year.append(int(chart.date[:4]))
 		artist_name.append(song.artist)
 		rank.append(song.rank)
 	chart = billboard.ChartData('hot-100', chart.previousDate)
-	count = count - 1
+	count = count + 1
 	print(count)
 
 df = pd.DataFrame({'year': year, 'artist_name': artist_name, 'rank': rank})
 df.to_csv('billboard_data.csv')
 print(df)
 
-# make delay 10s, delete count limit
+# make delay 10s

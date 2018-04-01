a83ab8691bc1b8a7b24d862186df59f9ef856f80
Luke Mainwaring <lukemainwaring@Lukes-MacBook-Pro-3.local>
2018-03-31 21:53:21 -0400

    final dataframe

--- every_noise_webscrape.py
+++ every_noise_webscrape.py
@@ -39,34 +39,18 @@ def main():
     del city_urls[35]
     del city_urls[6]
     del city_urls[4]
-    # city_urls.remove(6)
-    # city_urls.remove(4)
-    city_urls_test = city_urls[:5]
+    # city_urls_test = city_urls[:5]
     city_state_to_artists = {}
-    for city_url in city_urls_test:
-    # for city_url in city_urls:
+    # for city_url in city_urls_test:
+    for city_url in city_urls:
         city_name = city_url.split('=')[1].split('&')[0][:-2].split('%20')
         city_name = ' '.join(city_name).rstrip()
         # print(city_name)
         city_state_to_artists[city_name] = get_city_songs(city_url)
-    
-    # test_city_url = city_urls[1]
-    # print(test_city_url.split('='))
-    '''
-    city_name = test_city_url.split('=')[1].split('&')[0][:-2].split('%20')
-    city_name = ' '.join(city_name).rstrip()
-    print(city_name)
-
-    
-    city_state_to_artists[city_name] = get_city_songs(test_city_url)
-    print(city_state_to_artists)
-    '''
-    # print(city_state_to_artists)
-    # get_city_songs(test_city_url)
 
 
     # separate city_state to city and state
-    states = {'Alabama', 'Arizona', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida','Georgia', 'Hawaii', 'Idaho', 'Illinois', 
+    states = {'Alabama', 'Alaska', 'Arizona', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida','Georgia', 'Hawaii', 'Idaho', 'Illinois', 
     'Indiana', 'Iowa', 'Kentucky', 'Kansas', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 
     'Mississippi', 'Missouri', 'Nebraska', 'Nevada', 'New Jersey' ,'New Mexico', 'New York', 'North Carolina', 'Ohio', 
     'Oklahoma', 'Oregon', 'Pennsylvania', 'Puerto Rico','Rhode Island', 'South Carolina', 'Tennessee', 'Texas', 'Utah', 'Virginia', 'Washington', 'Wisconsin', 'Wyoming'}
@@ -74,7 +58,6 @@ def main():
     city_to_state = {}
     for city_state in city_state_to_artists.keys():
         city_info = city_state.split(' ')
-        
         # state with two words
         if city_info[-2] + ' ' +  city_info[-1] in states:
             state = city_info[-2] + ' ' +  city_info[-1]
@@ -87,24 +70,18 @@ def main():
         else:
             city = ' '.join(city_info)
             city_to_state[city] = 'N/A'     
-    print(city_to_state)
 
 
-    # convert dict to pandas dataframe
-    # df = pd.DataFrame.from_dict(city_state_to_artists, orient='index')
-    
-    df = pd.DataFrame(columns=['City', 'State', 'Artists'])
-    for data in city_state_to_artists.keys():
-        print(data)
-        data_info = data.split(' ')
+    # convert to pandas dataframe
+    rows_list = []
+    for city in city_to_state:
+        for city_state in city_state_to_artists.keys():
+            if city_state.startswith(city):
+                d = {'City': city, 'State': city_to_state[city], 'Artists': city_state_to_artists[city_state]}
+                rows_list.append(d)
 
-        if data_info[0] + ' ' + data_info[1] in city_to_state.keys():
-            print(data_info[])
-
-        # df.loc[i] = []
-
+    df = pd.DataFrame(rows_list)
     print(df)
-    
 
 
 if __name__ == "__main__":

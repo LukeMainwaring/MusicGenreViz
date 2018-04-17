70b70251d4223c848c5bf3172b5c3af1c3eaf793
Luke Mainwaring <lukemainwaring@huntsman-ve504-0363.apn.wlan.upenn.edu>
2018-04-16 22:05:14 -0400

    city and states file

--- clean_city_artists_file.py
+++ clean_city_artists_file.py
@@ -36,10 +36,22 @@ def remove_empty_cities(filename1, filename2, filename3, clean_filename):
                         visited.add(city_state)
                         f1.write(line)
 
+def create_city_state_list(filename, city_state_file):
+    with open(city_state_file, 'w') as f1, open(filename, 'r') as f2:
+        for line in f2:
+            city_info = line.split(',')
+            city_state = city_info[-2] + ',' + city_info[-1]
+            # print(city_state)
+            f1.write(city_state)
+
+
+def clean_spotify_artists_genres(filename, clean_filename):
+    pass
 
 def main():
-    remove_empty_cities("city_artists_file.txt", "city_artists_file2.txt", 
-        "city_artists_file3.txt", "city_artists_file_cleaned.txt")
+    # remove_empty_cities("city_artists_file.txt", "city_artists_file2.txt", 
+    #    "city_artists_file3.txt", "city_artists_file_cleaned.txt")
+    create_city_state_list('city_artists_file_cleaned.txt', 'city_states.txt')
 
 
 if __name__ == "__main__":

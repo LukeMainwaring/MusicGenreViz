0a4cca73d77adf3b182455a90a1d1321266f4d0d
Luke Mainwaring <lukemainwaring@huntsman-ve504-0363.apn.wlan.upenn.edu>
2018-04-16 22:41:11 -0400

    city states with lat and long

--- clean_city_artists_file.py
+++ clean_city_artists_file.py
@@ -36,14 +36,90 @@ def remove_empty_cities(filename1, filename2, filename3, clean_filename):
                         visited.add(city_state)
                         f1.write(line)
 
-def create_city_state_list(filename, city_state_file):
-    with open(city_state_file, 'w') as f1, open(filename, 'r') as f2:
+def create_city_state_list(filename, city_state_file, geodata_file):
+
+    states = {
+            'AK': 'Alaska',
+            'AL': 'Alabama',
+            'AR': 'Arkansas',
+            'AS': 'American Samoa',
+            'AZ': 'Arizona',
+            'CA': 'California',
+            'CO': 'Colorado',
+            'CT': 'Connecticut',
+            'DC': 'District of Columbia',
+            'DE': 'Delaware',
+            'FL': 'Florida',
+            'GA': 'Georgia',
+            'GU': 'Guam',
+            'HI': 'Hawaii',
+            'IA': 'Iowa',
+            'ID': 'Idaho',
+            'IL': 'Illinois',
+            'IN': 'Indiana',
+            'KS': 'Kansas',
+            'KY': 'Kentucky',
+            'LA': 'Louisiana',
+            'MA': 'Massachusetts',
+            'MD': 'Maryland',
+            'ME': 'Maine',
+            'MI': 'Michigan',
+            'MN': 'Minnesota',
+            'MO': 'Missouri',
+            'MP': 'Northern Mariana Islands',
+            'MS': 'Mississippi',
+            'MT': 'Montana',
+            'NA': 'National',
+            'NC': 'North Carolina',
+            'ND': 'North Dakota',
+            'NE': 'Nebraska',
+            'NH': 'New Hampshire',
+            'NJ': 'New Jersey',
+            'NM': 'New Mexico',
+            'NV': 'Nevada',
+            'NY': 'New York',
+            'OH': 'Ohio',
+            'OK': 'Oklahoma',
+            'OR': 'Oregon',
+            'PA': 'Pennsylvania',
+            'PR': 'Puerto Rico',
+            'RI': 'Rhode Island',
+            'SC': 'South Carolina',
+            'SD': 'South Dakota',
+            'TN': 'Tennessee',
+            'TX': 'Texas',
+            'UT': 'Utah',
+            'VA': 'Virginia',
+            'VI': 'Virgin Islands',
+            'VT': 'Vermont',
+            'WA': 'Washington',
+            'WI': 'Wisconsin',
+            'WV': 'West Virginia',
+            'WY': 'Wyoming'
+    }
+
+    state_codes = {v: k for k, v in states.items()}
+    # print(state_codes)
+    city_states = set()
+    visited = set()
+    
+    with open(city_state_file, 'w') as f1, open(filename, 'r') as f2, open(geodata_file, 'r') as f3:
         for line in f2:
             city_info = line.split(',')
-            city_state = city_info[-2] + ',' + city_info[-1]
-            # print(city_state)
-            f1.write(city_state)
+            if city_info[-1] != 'State\n' and city_info[-1] != 'N/A\n':
+                city_state = city_info[-2] + ',' + state_codes[city_info[-1].strip('\n')]
+                city_states.add(city_state)
 
+        for line in f3:
+            city_data = line.split(',')
+            lat, lon = city_data[1], city_data[2][1:-1]
+            city_name, state_name = city_data[3][1:-1], city_data[4][1:-1]
+            city_state = city_name + ',' + state_name
+            if city_state in city_states and city_state not in visited:
+                visited.add(city_state)
+                f1.write(city_state + ',' + lat + ',' + lon + '\n')
+
+    
 
 def clean_spotify_artists_genres(filename, clean_filename):
     pass
@@ -51,7 +127,8 @@ def clean_spotify_artists_genres(filename, clean_filename):
 def main():
     # remove_empty_cities("city_artists_file.txt", "city_artists_file2.txt", 
     #    "city_artists_file3.txt", "city_artists_file_cleaned.txt")
-    create_city_state_list('city_artists_file_cleaned.txt', 'city_states.txt')
+    
+    # create_city_state_list('city_artists_file_cleaned.txt', 'city_states.txt', 'zip_codes_states.csv')
 
 
 if __name__ == "__main__":

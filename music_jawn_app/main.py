cca498247f103793630abdbb5e6224dd524d5190
evan <ejonokuchi@gmail.com>
2018-04-18 17:03:40 -0400

    Reorganized files, added basic flask app skeleton

---
+++ music_jawn_app/main.py
@@ -0,0 +1,35 @@
+'''
+music_jawn_app.main
+-------------------
+Starting point and main functionality of music jawn app.
+
+'''
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    context = {
+        'title': 'Home',
+    }
+    return render_template('home.html', **context)
+
+
+@app.route("/map/")
+def map():
+    context = {
+        'title': 'Map',
+    }
+    return render_template('map.html', **context)
+
+
+@app.route("/timeline/")
+def timeline():
+    context = {
+        'title': 'Timeline',
+    }
+    return render_template('timeline.html', **context)

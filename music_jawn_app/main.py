b63098c0f8c039374265592b5d0816341b025a08
Luke Mainwaring <lukemainwaring@huntsman-ve507-0980.apn.wlan.upenn.edu>
2018-04-18 19:15:19 -0400

    added bootstrap

--- music_jawn_app/main.py
+++ music_jawn_app/main.py
@@ -7,8 +7,10 @@ Starting point and main functionality of music jawn app.
 
 
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
 @app.route("/")

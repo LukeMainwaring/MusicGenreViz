5dc0f22fd7cc7acd08a4ba4fc31b0a33217a3cd3
Hannah Stulberg <stulberg@wharton.upenn.edu>
2018-04-22 21:59:49 -0400

    js with logging

--- music_jawn_app/static/js/main.js
+++ music_jawn_app/static/js/main.js
@@ -2,9 +2,11 @@ var slider = document.getElementById("myRange");
 //var output = document.getElementById("demo");
 //output.innerHTML = slider.value; // Display the default slider value
 $(".demo").text(slider.value)
+console.log(slider.value)
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
     //output.innerHTML = this.value;
     $(".demo").text(slider.value)
+    console.log(slider.value)
 }

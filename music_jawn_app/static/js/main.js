d621e60baf1bc6f3fd84dc1c182ea65a69ff25d8
Luke Mainwaring <lukemainwaring@huntsman-ve508-0799.apn.wlan.upenn.edu>
2018-04-22 23:15:57 -0400

    jfdskfj

--- music_jawn_app/static/js/main.js
+++ music_jawn_app/static/js/main.js
@@ -1,3 +1,4 @@
+console.log("js")
 var slider = document.getElementById("myRange");
 //var output = document.getElementById("demo");
 //output.innerHTML = slider.value; // Display the default slider value
